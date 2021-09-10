46cbbea90c1a8c7297cfb1d031130f2cd9254e04
tannerlsmith <tanner.smith@uvm.edu>
2021-09-10 17:53:00 -0400

    debug

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';import Login from './pages/Login';
+import Home from './pages/Home';
+import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
-
-import Home from './pages/Home';
 
 const client = new ApolloClient({
   uri: '/graphql'
@@ -22,18 +21,22 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/profile/:username?" component={Profile} />
-          <Route exact path="/thought/:id" component={SingleThought} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>      
-  </Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
 
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
 const Home = () => {
-  // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
 
   return (
     <main>
@@ -20,9 +21,6 @@ const Home = () => {
       </div>
     </main>
   );
-
-  const thoughts = data?.thoughts || [];
-  console.log(thoughts);
 };
 
 export default Home;
