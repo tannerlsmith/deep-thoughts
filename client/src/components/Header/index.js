c5a72a0ec67933dffed8c7c7fddd859b57288f80
tannerlsmith <tanner.smith@uvm.edu>
2021-09-11 18:12:00 -0400

    updated header for user contextualized data

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
   return (
@@ -9,9 +10,21 @@ const Header = () => {
           <h1>Deep Thoughts</h1>
         </Link>
 
+      
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+            {Auth.loggedIn() ? (
+              <>
+                <Link to="/profile">Me</Link>
+                <a href="/">
+                  Logout
+                </a>
+              </>
+            ) : (
+              <>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+              </>
+            )}
         </nav>
       </div>
     </header>
